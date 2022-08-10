5934cbb90172070bb4713d64ed9b671ec36a414a
Paul-Bett <bettpaul6@gmail.com>
2022-08-11 00:13:42 +0300

    update

--- shelldone/launch.c
+++ shelldone/launch.c
@@ -1,8 +1,8 @@
 #include "shell.h"
 /**
   * shell_launch - Launch a program and wait for it to terminate.
-  * @args - Null terminated list of arguments (including program)
-  * return: Always returns 1, to continue execution.
+  * @args: Null terminated list of arguments (including program)
+  * Return: Always returns 1, to continue execution.
   */
 int shell_launch(char **args)
 {
@@ -12,17 +12,17 @@ int shell_launch(char **args)
 	pid = fork();
 	if (pid == 0)
 	{
-	// Child process
+	/* Child process */
 		if (execvp(args[0], args) == -1)
 			perror("./shell");
 	exit(EXIT_FAILURE);
 	}
 	else if (pid < 0)
-	// Error forking
+	/* Error forking */
 		perror("./shell");
 	else
 	{
-	// Parent process
+	/* Parent process */
 		do {
 			waitpid(pid, &status, WUNTRACED);
 		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
